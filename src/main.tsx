19d272329fc34c7d48fed64ff0e35d4f7954765d
Ronskieee <jamesarronn@gmail.com>
2026-04-25 23:44:25 +0800

    Change import path for App component
    Updated import path for App component to include 'app/' prefix.

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/index.css'   // ← dapat may 'styles/' prefix!
-import App from './App'
+import App from './app/App'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
